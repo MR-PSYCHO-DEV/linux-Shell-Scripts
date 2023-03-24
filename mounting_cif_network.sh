68572326c40667f6f1ecdfac0793366b26f9d6a4
MR-PSYCHO-DEV <83563243+MR-PSYCHO-DEV@users.noreply.github.com>
2023-03-25 08:11:16 +1300

    Update mounting_cif_network.sh
    More easier to read where to put username, password and IP

--- mounting_cif_network.sh
+++ mounting_cif_network.sh
@@ -1,1 +1,1 @@
-sudo mount -t cifs -o username=username,password=password,uid=1000,gid=100,dir_mode=0770,file_mode=0660 //nas/path /device/path
+sudo mount -t cifs -o username=username_here,password=password_here,uid=1000,gid=100,dir_mode=0770,file_mode=0660 //IP_of_nas/path /Device/path
